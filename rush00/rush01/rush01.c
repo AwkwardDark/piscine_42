f0808a7fafe4eb66a25178adb2ad2f0e09d57c17
AwkwardDark <81935127+AwkwardDark@users.noreply.github.com>
2024-03-20 11:25:55 +0100

    Add files via upload

---
+++ rush00/rush01/rush01.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush01.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pajimene <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/16 10:03:50 by pajimene          #+#    #+#             */
+/*   Updated: 2024/03/17 12:09:22 by pajimene         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char a);
+
+void	premiere_ligne(int i, int x)
+{
+	if (i == 1)
+		ft_putchar('/');
+	else if ((i > 1) && (i < x))
+		ft_putchar('*');
+	else if (i == x)
+		ft_putchar('\\');
+}
+
+void	inter_ligne(int i, int x)
+{
+	if ((i == 1) || (i == x))
+		ft_putchar('*');
+	else if ((i > 1) && (i < x))
+		ft_putchar(' ');
+}
+
+void	derniere_ligne(int i, int x)
+{
+	if (i == 1)
+		ft_putchar('\\');
+	else if ((i > 1) && (i < x))
+		ft_putchar('*');
+	else if (i == x)
+		ft_putchar('/');
+}
+
+void	print_ligne(int i, int j, int x, int y)
+{
+	while (i <= x)
+	{
+		if (j == 1)
+		{
+			premiere_ligne(i, x);
+		}
+		else if ((j > 1) && (j < y))
+		{
+			inter_ligne(i, x);
+		}
+		else if (j == y)
+		{
+			derniere_ligne(i, x);
+		}
+		i++;
+	}	
+}
+
+void	rush(int x, int y)
+{
+	int	i;
+	int	j;
+
+	j = 1;
+	while (j <= y)
+	{
+		i = 1;
+		print_ligne(i, j, x, y);
+		ft_putchar('\n');
+		j++;
+	}
+}
