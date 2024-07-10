aa1a2371e0db987219c7e0f5234d36a751fb9a92
AwkwardDark <81935127+AwkwardDark@users.noreply.github.com>
2024-07-10 13:08:34 +0400

    Update ft_sort_int_tab.c

--- c01/ex08/ft_sort_int_tab.c
+++ c01/ex08/ft_sort_int_tab.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pajimene <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: marvin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 11:41:05 by pajimene          #+#    #+#             */
 /*   Updated: 2024/03/16 15:48:51 by pajimene         ###   ########.fr       */
