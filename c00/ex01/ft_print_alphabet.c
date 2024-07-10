e86e421f1a054781de66e5b95ae7899a27d0606e
AwkwardDark <81935127+AwkwardDark@users.noreply.github.com>
2024-07-10 12:59:48 +0400

    Update ft_print_alphabet.c

--- c00/ex01/ft_print_alphabet.c
+++ c00/ex01/ft_print_alphabet.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_alphabet.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pajimene <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: marvin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 11:26:29 by pajimene          #+#    #+#             */
 /*   Updated: 2024/03/14 10:44:05 by pajimene         ###   ########.fr       */
