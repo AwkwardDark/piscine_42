e4d0ad36b9755371b3b6389bed23d9dfee7d5fa5
AwkwardDark <81935127+AwkwardDark@users.noreply.github.com>
2024-07-10 13:00:01 +0400

    Update ft_print_reverse_alphabet.c

--- c00/ex02/ft_print_reverse_alphabet.c
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pajimene <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: marvin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 11:42:29 by pajimene          #+#    #+#             */
 /*   Updated: 2024/03/14 11:30:37 by pajimene         ###   ########.fr       */
